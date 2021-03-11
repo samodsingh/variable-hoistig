cf985c7366e8fbb016879aaafcd95f244697601d
samod singh <samod0098@gmail.com>
2021-03-11 19:47:53 +0530

    variable-hoistig

---
+++ my.js
@@ -0,0 +1,13 @@
+var a =10;
+document.write(a + "" + b + "<br>");
+var b = 20;
+document.write(a + " " + b );
+
+/*
+var a;
+var b;
+a =10;
+document.write(a + " " + b);
+b =20;
+document.write(a + " " + b );
+*/
